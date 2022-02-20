64f83e174e5546892366aab481d2aac62b41454b
Anay <anay0@icloud.com>
2022-02-20 12:24:22 -0700

    add: list view

--- swiftui-list-app/ContentView.swift
+++ swiftui-list-app/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("item 1")
+                Text("item 2")
+                Text("item 3")
+                Text("item 4")
+            }.navigationBarTitle(Text("List App"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
